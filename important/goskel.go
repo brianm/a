fcd49adb64aabeaab10688d0b56e217ef32d20d0
Brian McCallister <brianm@skife.org>
2013-04-30 13:05:57 -0600

    go fmt

--- important/goskel.go
+++ important/goskel.go
@@ -5,9 +5,7 @@ import (
 
 	// imported solely so go get has somethign to do	
 	_ "github.com/brianm/variant"
-	
 )
-
 
 func Greet(name string) (int, error) {
 	return fmt.Printf("Hello %s!\n", name)
